39d3c49ef69ede56cbabbd650a6fa88e25b8c786
MehmetBeyazgul <12203839@student.pxl.be>
2024-10-03 11:36:36 +0200

    vereisten demo project

---
+++ projectVeresten/projectVeresten/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace projectVeresten
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
